5ea723bb3a4a0ab346c4590c6b061abc1c873a90
SkibstedN <niko933n@stud.kea.dk>
2023-04-25 17:12:29 +0200

    Created and tested the database connection with success.

--- server/app.js
+++ server/app.js
@@ -1,2 +1,53 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+
+  async function testDBConnection() {
+    try {
+      const connection = await pool.getConnection();
+      console.log("Connected to the database.");
+      connection.release();
+      return true;
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      return false;
+    }
+  }
+
+  app.get("/test-db-connection", async (req, res) => {
+    const isConnected = await testDBConnection();
+    if (isConnected) {
+      res.status(200).json({ message: "Connected to the database." });
+    } else {
+      res.status(500).json({ message: "Failed to connect to the database." });
+    }
+  });
+
+  // Add routes here
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
